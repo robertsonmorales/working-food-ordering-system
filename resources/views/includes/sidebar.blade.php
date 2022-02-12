037081b1f528c70d3ad97e0d31bb9e9dd96a9e5c
Robertson Morales <moralesso152842@gmail.com>
2022-02-13 06:26:55 +0800

    fixed logout

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -9,42 +9,51 @@
         <div class="nav-list">
             <ul class="list">
                 <li class="list-item">
-                    <a href="#dashboard" title="dashboard" class="btn-nav-item">
+                    <a href="#dashboard" 
+                        title="dashboard" 
+                        class="btn-nav-item">
                         <i data-feather="bar-chart-2"></i>
                     </a>
                 </li>
                 <li class="list-item">
-                    <a href="#menu" title="menu" class="btn-nav-item active">
+                    <a href="#menu" 
+                        title="menu"
+                        class="btn-nav-item">
                         <i data-feather="activity"></i>
                     </a>
                 </li>
                 <li class="list-item">
-                    <a href="#orders" title="orders" class="btn-nav-item">
+                    <a href="#orders" 
+                        title="orders"
+                        class="btn-nav-item">
                         <i data-feather="book-open"></i>
                     </a>
                 </li>
                 <li class="list-item">
-                    <a href="#inventories" title="inventories" class="btn-nav-item">
+                    <a href="#inventories" 
+                        title="inventories" 
+                        class="btn-nav-item">
                         <i data-feather="archive"></i>
                     </a>
                 </li>
                 <li class="list-item">
-                    <a href="#reports" title="reports" class="btn-nav-item">
+                    <a href="#reports" 
+                        title="reports" 
+                        class="btn-nav-item">
                         <i data-feather="database"></i>
                     </a>
                 </li>
             </ul>
         </div>
         <div class="user">
-            <a class="btn btn-user logout" href="#logout">
-                <i data-feather="log-out"></i>
-            </a>
-
             <form id="logout-form" 
                 action="{{ route('logout') }}" 
-                method="POST" 
-                class="d-none">
+                method="POST">
                 @csrf
+
+                <button type="button" class="btn btn-user btn-logout">
+                    <i data-feather="log-out"></i>
+                </button>
             </form>
         </div>
     </nav>
